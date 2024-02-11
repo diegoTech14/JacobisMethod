f744723b51b39663bab6eadcb2c9e02e2ea4efc3
Denny Gutrie <gutriedenny@gmail.com>
2024-02-11 14:07:00 -0600

    Despejes y acomodo de ecuaciones

--- MetodoJacobi.py
+++ MetodoJacobi.py
@@ -9,13 +9,60 @@ x_jacobi = 0;
 y_jacobi = 0;
 z_jacobi = 0;
 
-def obtener_valores(ecuacion):
-    list = [int(numero) for numero in re.findall(r"-?\d+\.?\d*",ecuacion)]
-    return list
 
-def obtener_variables(ecuacion):
-    list_var = re.findall('[a-zA-Z]',ecuacion)
-    return sorted(list_var)
+def ordenar_ecuacion(ecuacion):
+    ecuacion_ordenada = [0,0,0,0] #SE CREA UN VECTOR [X,Y,Z,C], DE MANERA QUE SI FALTA ALGUNA VARIABLE, LA MISMA QUEDA EN 0 POR DEFECTO
+    for termino in ecuacion: #LA ECUACION LLEGA EN ESTA FORMA ['-2x', 'z', '3y', '20']
+        if termino[-1] == "x": #CON EL [-1] ME POSICIONO EN EL ÚLTIMO CARACTER DEL STRING OBTENIDO DEL ELEMENTO DEL VECTOR EXTRAIDO EN EL FOR "-2x" -> "x"
+            if termino[:-1] == "-":#CON EL [:-1] ME POSICIONO EN LOS CARATERES DEL STRING OBTENIDO DEL ELEMENTO DEL VECTOR EXTRAIDO EN EL FOR OMITIENDO LA ULTIMA POSICION  "-2x" -> "-2"
+                ecuacion_ordenada[0] = -1
+                continue
+            if termino[:-1] == "":#SI NO HAY UN VALOR ANTES DEL ÚLTIMO, ME GENERA UN CARACTER VACÍO "z" -> ""
+                ecuacion_ordenada[0] = 1 #SE GENERA UN 1 REPRESENTANDO QUE SOLO EXISTE LA VARIABLE
+                continue
+            ecuacion_ordenada[0] = int(termino[:-1])#SI LA VARIABLE NO ESTÁ SOLA O EN ESTAS FORMAS "-x" - "y", ENTONCES AGREGAMOS EL VALOR QUE LAS ACOMPAÑA A LA ECUACIÓN ORDENADA
+        if termino[-1] == "y":
+            if termino[:-1] == "-":
+                ecuacion_ordenada[1] = -1
+                continue
+            if termino[:-1] == "":
+                ecuacion_ordenada[1] = 1
+                continue
+            ecuacion_ordenada[1] = int(termino[:-1])
+        if termino[-1] == "z":
+            if termino[:-1] == "-":
+                ecuacion_ordenada[2] = -1
+                continue
+            if termino[:-1] == "":
+                ecuacion_ordenada[2] = 1
+                continue
+            ecuacion_ordenada[2] = int(termino[:-1])
+    ecuacion_ordenada[3] = int(ecuacion[-1])
+    return ecuacion_ordenada #DEVUELVE EL VECTOR DIRECTOR DE LA ECUACIÓN
+#print(ordenar_ecuacion(['x', '3z', '5y', '3']))
+
+
+def separar_terminos_ecuacion(string_ecuacion):
+    lista_ecuacion = []
+    string_temp = ""
+    for caracter in string_ecuacion:
+        if caracter == "-": #SI EL CARACTER CORRESPONDE A UN NEGATIVO
+            if string_temp == "": #VERIFICAMOS QUE EL STRING TEMPORAL ESTÉ VACÍO Y LE ASIGNAMOS UN NEGATIVO
+                string_temp = "-" #SE LE ASIGNA UN NEGATIVO PORQUE AL TERMINAR DE VERIFICAR LOS IF, SE LE VA A AGREGAR EL TERMINO QUE COMPLETA ESE NEGATIVO
+                continue
+            lista_ecuacion += [string_temp]#EN CASO DE QUE EL STRING TEMP NO ESTÉ VACÍO, SE PROCEDE A AÑADIR EL STRING TEMPORAL FORMADO A LA LISTA DE LA ECUACION SEPARADA
+            string_temp = "" #SE VUELVE A VACIAR EL STRING TEMPORAL PARA LLENARLO DE NUEVO
+        if caracter == "+":
+            lista_ecuacion += [string_temp]
+            string_temp = ""
+            continue
+        if caracter == "=":
+            lista_ecuacion += [string_temp]
+            string_temp = ""
+            continue
+        string_temp += caracter #SI SE VERIFICA TODOS LOS IF Y NO HAY ALGUN SIGNO DE OPERACIÓN O IGUALDAD, SE LE AÑADE AL STRING TEMPORAL EL NUMERO O LETRA QUE CORRESPONDA
+    lista_ecuacion += [string_temp] #AL TERMINAR EL FOR, SE AÑADE LO ÚLTIMO QUE SE AGREGÓ AL STRING TEMPORAL PARA NO DEJAR POR FUERA LOS ULTIMOS TERMINOS DE LA ECUACION
+    return lista_ecuacion
 
 def obtener_mayor(list):
     listadosNumero = [num for num in list[:3]]
@@ -39,10 +86,9 @@ def obtener_igualdad(list):
 
 def matriz_var(ecuacion):
     var_matriz = []
-    var_matriz.append(obtener_variables(ecuacion))
+    var_matriz.append(ordenar_ecuacion(separar_terminos_ecuacion(ecuacion)))
     print(var_matriz)
-
-
+    
 def despejar_incognitas(list, igualdad, bandera):
     x, y, z = sp.symbols('x y z')
 
@@ -59,7 +105,6 @@ def despejar_incognitas(list, igualdad, bandera):
         variable = z
     
     solucion = sp.solve(ecuacion, variable, dict=True)
-    
     return solucion[0][variable]
 
 def ordenar_matriz(matriz):
@@ -97,20 +142,22 @@ def formar_matriz(ec1,ec2,ec3):
     matriz=[]
     for i in range(3):
         if i == 0:
-            matriz.append(obtener_cocientes(obtener_valores(ec1)))
+            matriz.append(ordenar_ecuacion(separar_terminos_ecuacion(ec1))[:3])
         elif i == 1:
-            matriz.append(obtener_cocientes(obtener_valores(ec2)))
+            matriz.append(ordenar_ecuacion(separar_terminos_ecuacion(ec2))[:3])
         elif i == 2:
-            matriz.append(obtener_cocientes(obtener_valores(ec3)))
-
+            matriz.append(ordenar_ecuacion(separar_terminos_ecuacion(ec3))[:3])
+            
     igualdades[0] = obtener_igualdad(ec1)
     igualdades[1] = obtener_igualdad(ec2)
     igualdades[2] = obtener_igualdad(ec3)
     
     matriz = ordenar_matriz(matriz)
-    despejes[0]=despejar_incognitas(obtener_cocientes(obtener_valores(matriz[0])), int(igualdades[0][1:]), 0)
-    despejes[1]=despejar_incognitas(obtener_cocientes(obtener_valores(matriz[1])), int(igualdades[1][1:]), 1)  
-    despejes[2]=despejar_incognitas(obtener_cocientes(obtener_valores(matriz[2])), int(igualdades[2][1:]), 2)
+    
+    print(matriz[0])
+    despejes[0]=despejar_incognitas(ordenar_ecuacion(separar_terminos_ecuacion(matriz[0])), int(igualdades[0][1:]), 0)
+    despejes[1]=despejar_incognitas(ordenar_ecuacion(separar_terminos_ecuacion(matriz[1])), int(igualdades[1][1:]), 1)  
+    despejes[2]=despejar_incognitas(ordenar_ecuacion(separar_terminos_ecuacion(matriz[2])), int(igualdades[2][1:]), 2)
 
 def calcularError(valorAnterior,aproximacion):
     error = np.abs((float(valorAnterior)-float(aproximacion)))
@@ -139,9 +186,9 @@ def jacobi(x_in,y_in,z_in,error_max,iteraciones):
     y_jacobi = y_in
     z_jacobi = z_in
     #Formulas de los despejes
-    x_form = '(3-y-2*z)/10'
-    y_form = '(9-4*x+z)/6'
-    z_form = '(51+2*x-3*y)/8'
+    x_form = despejes[0]
+    y_form = despejes[1]
+    z_form = despejes[2]
     print(x_form)
     print(y_form)
     print(z_form)
@@ -164,10 +211,9 @@ def jacobi(x_in,y_in,z_in,error_max,iteraciones):
         else:
           print(f"Iteración {i}: X: {x_jacobi}\tY: {y_jacobi}\tZ: {z_jacobi}")
 
+print("Despeje Automatizado")
 formar_matriz("10x+y+2z=3",
-              "4x+6y-z=9",
-              "-2x+3y+8z=51")
+             "4x+6y-z=9",
+            "-2x+3y+8z=51")
 
 jacobi(0,0,0,0.02,6)
-
-#calcularError("-0.04569484","-0.04518692")
